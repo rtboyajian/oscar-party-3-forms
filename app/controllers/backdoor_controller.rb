7ede0359905b8da5c387abbe26a45e0d8557563c
rtboyajian <rtboyajian@gmail.com>
2025-09-12 00:21:18 +0000

    commit

--- app/controllers/backdoor_controller.rb
+++ app/controllers/backdoor_controller.rb
@@ -34,6 +34,25 @@ class BackdoorController < ApplicationController
     end
   end
 
+  def update_director
+    the_id = params.fetch("path_id")
+    the_director = Director.where({:id => the_id}).at(0)
+
+    the_director.first_name = params.fetch("query_first_name")
+    the_director.last_name = params.fetch("query_last_name")
+    the_director.dob = params.fetch("query_dob")
+    the_director.bio = params.fetch("query_bio")
+    the_director.image = params.fetch("query_image")
+
+    if the_director.valid?
+      the_director.save
+      redirect_to("/backdoor/directors/#{the_director.id}", {:notice => "Director updated successfully."})
+    else
+        redirect_to("/backdoor/directors/#{the_director.id}", {:alert => the_director.errors.full_messages.to_sentence})
+    end
+  end
+    
+
   def destroy_director 
     the_id = params.fetch("/path_id")
     the_director = Director.where({:id => the_id}).at(0)
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get("/backdoor/directors", {:controller => "backdoor", :action => "directors_index"})
   get("/backdoor/directors/:path_id", {:controller => "backdoor", :action => "director_show"})
   post("/backdoor/insert_director", {:controller => "backdoor", :action => "create_director"})
+  post("/backdoor/modify_director/:path_id", {:controller => "backdoor", :action => "update_director"})
   get("/backdoor/delete_director/:path_id", {:controller => "backdoor", :action => "destroy_director"})
 end
